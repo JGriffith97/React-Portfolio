2de8815d351f6d110f5273942008bf072e581aa4
Jayden Griffith <griffithjayden97@outlook.com>
2022-09-07 20:01:51 -0600

    Added css files and imports to their react components.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../../css/About.css';
 
 export default function About() {
   return (
+    // Can add some different css styles if need be.
     <div>
       <h1>
         {/* Work In Progress */}
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../../css/Projects.css';
 
 export default function Projects() {
   return (

9b0cbbb91e10fbc720de81f9725e72e937d84493
Jayden Griffith <griffithjayden97@outlook.com>
2022-09-12 21:52:34 -0600

    Made the portfolio page.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Me from '../../images/me.jpg'
 
+// NOTES-TODOs
+// Need to figure out how to fill the rest of the page so 
+// the footer stays on the bottom of the page.
+
 export default function About() {
   return (
     // Can add some different css styles if need be.
@@ -9,13 +13,13 @@ export default function About() {
         <h2>
           About Me
         </h2>
-        <img className='me-img' src={Me} alt='Myself, Jayden Griffith'></img>
-        <p className='main-paragraph'>
-          Hello, my name is Jayden Griffith, currently a student of the University of Utah's Coding Bootcamp. Which began in April, 2022.
-
-          I love basketball, hockey, gaming, and I'm becoming quite passionate about coding as well!
-        </p>
       </div>
+      <img className='me-img' src={Me} alt='Myself, Jayden Griffith'></img>
+      <p className='main-area'>
+        Hello, my name is Jayden Griffith, currently a student of the University of Utah's Coding Bootcamp. Which began in April, 2022.
+
+        I love basketball, hockey, gaming, and I'm becoming quite passionate about coding as well!
+      </p>
     </div>
   )
 }
